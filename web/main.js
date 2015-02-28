91ef236043be855c057c1614acce2ce7a40686c1
Willis F. Jackson III <raidercat@gmail.com>
2015-02-28 16:14:55 -0600

    fixed your awful indents

--- web/main.js
+++ web/main.js
@@ -8,38 +8,38 @@ var host = "http://demo.bigbang.io";
 
 client.connect(host, function (err) {
 
-    if (err) {
-        alert("Failed to connect to " + host);
-        return;
-    }
+  if (err) {
+    alert("Failed to connect to " + host);
+    return;
+  }
 
-    client.subscribe("helloWebRTC", function (err, channel) {
-        if (err) {
-            alert("Unable to subscribe to channel");
-            return;
-        }
-        console.log('subscribed!');
-        var webrtc = new SimpleWebRTC({
-            // the id/element dom element that will hold "our" video
-            localVideoEl: 'localVideo',
-            // the id/element dom element that will hold remote videos
-            remoteVideosEl: 'remotesVideos',
-            // immediately ask for camera access
-            autoRequestMedia: true
-        });
+  client.subscribe("helloWebRTC", function (err, channel) {
+    if (err) {
+      alert("Unable to subscribe to channel");
+      return;
+    }
+    console.log('subscribed!');
+    var webrtc = new SimpleWebRTC({
+      // the id/element dom element that will hold "our" video
+      localVideoEl: 'localVideo',
+      // the id/element dom element that will hold remote videos
+      remoteVideosEl: 'remotesVideos',
+      // immediately ask for camera access
+      autoRequestMedia: true
+    });
 
 
 // we have to wait until it's ready
-        webrtc.on('readyToCall', function () {
-            console.log('ready!');
-            // you can name it anything
-            webrtc.joinRoom('yargladfj24r8a');
-        });
-
-
-        //beginChat(channel);
-
+    webrtc.on('readyToCall', function () {
+      console.log('ready!');
+      // you can name it anything
+      webrtc.joinRoom('yargladfj24r8a');
     });
+
+
+    //beginChat(channel);
+
+  });
 });
 
 
